94a6ec64a828b1e25cbdbf008ed014af2e35746d
userMS <marcosae@hotmail.it>
2021-03-12 12:19:14 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import os
+
+from datetime import datetime
+from time import mktime
+
+import setuptools
+
+
+dt = datetime.fromisoformat(os.getenv("CI_COMMIT_TIMESTAMP"))
+offset = dt.tzinfo.utcoffset(dt).seconds
+timestamp = round(mktime(dt.timetuple()) - offset)
+setuptools.setup(
+    name=os.getenv("PACKAGE_NAME"),
+    version="{}".format(os.getenv("PACKAGE_VERSION")),
+    url="https://github.com/userMS/test-project",
+    author="userMS",
+    author_email="marcosae@hotmail.it",
+    description="Testing github package",
+    packages=setuptools.find_packages(),
+    install_requires=[
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
